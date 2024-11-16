d67bbc0627a0800ba55a8530108be9e864796cb4
felipemadu13 <felipemadureira.a@hotmail.com>
2024-11-16 19:25:49 -0300

    fix: correção cors

--- api_root/settings.py
+++ api_root/settings.py
@@ -45,12 +45,12 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'api_root.urls'
@@ -128,8 +128,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # CORS_ORIGIN_ALLOW_ALL = True
 
-CORS_ALLOW_ORIGINS = [
-    'http://localhost:8000',
-    'http://localhost:8080',
-    'http://localhost:4200',
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:4200", 
 ]
